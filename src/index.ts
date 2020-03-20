3baa45edc7b43d4667654f6d62ca7238d76ee1d9
Noah Crowley <noahwillcrow@gmail.com>
2020-03-20 01:36:52 -0400

    Add initial files to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export function makeHello(name: string) {
-	return `Hello from ${name}!`;
-}
+// Interfaces
+export { IRenderer } from "./Interfaces/IRenderer";
+export { IViewportRegion } from "./Interfaces/IViewportRegion";
+
+// Implementations
+export * from "./Implementation/Renderers/FilledCircleRenderer";
+export * from "./Implementation/ViewportRegions/CircleViewportRegion";
+export * from "./Implementation/ViewportRegions/RectangleViewportRegion";
