051258703390046883bea94bd382ac2fbd0923d1
Noah Crowley <noahwillcrow@gmail.com>
2020-04-06 14:18:15 -0400

    clean up

--- src/Interfaces/IJoystickConfiguration.d.ts
+++ src/Interfaces/IJoystickConfiguration.d.ts
@@ -3,36 +3,36 @@ import { IJoystickRenderer } from "./IJoystickRenderer";
 
 /** The interface for constructing a new `IJoystick` */
 export interface IJoystickConfiguration {
-    /** Defines the screen region in which the `IJoystick` instance can be activated by the player's input */
-    activationRegion: IGuiWindowRegion;
+	/** Defines the screen region in which the `IJoystick` instance can be activated by the player's input */
+	activationRegion: IGuiWindowRegion;
 
-    /** Defines the gutter radius in pixels */
-    gutterRadiusInPixels: number;
+	/** Defines the gutter radius in pixels */
+	gutterRadiusInPixels: number;
 
-    /** Defines the inactive center point in the viewport of the `IJoystick` instance */
-    inactiveCenterPoint: Vector2;
+	/** Defines the inactive center point in the viewport of the `IJoystick` instance */
+	inactiveCenterPoint: Vector2;
 
-    /**
-     * Defines whether the `IJoystick` instance should initialize as enabled.
-     * Defaults to false.
-     */
-    initializedEnabled?: boolean;
+	/**
+	 * Defines whether the `IJoystick` instance should initialize as enabled.
+	 * Defaults to false.
+	 */
+	initializedEnabled?: boolean;
 
-    /**
-     * Defines whether the `IJoystick` instance should initialize as visible.
-     * Defaults to false.
-     */
-    initializedVisible?: boolean;
+	/**
+	 * Defines whether the `IJoystick` instance should initialize as visible.
+	 * Defaults to false.
+	 */
+	initializedVisible?: boolean;
 
-    /**
-     * Defines the priority leve lof the `IJoystick` instance.
-     * Defaults to 1.
-     */
-    priorityLevel?: number;
+	/**
+	 * Defines the priority leve lof the `IJoystick` instance.
+	 * Defaults to 1.
+	 */
+	priorityLevel?: number;
 
-    /** Defines the renderer instance for the `IJoystick` instance */
-    renderer: IJoystickRenderer;
+	/** Defines the renderer instance for the `IJoystick` instance */
+	renderer: IJoystickRenderer;
 
-    /** Defines the thumb radius as a number in the range [0, 1] relative to the gutter radius */
-    relativeThumbRadius: number;
+	/** Defines the thumb radius as a number in the range [0, 1] relative to the gutter radius */
+	relativeThumbRadius: number;
 }
