250bbbff0a2a34ac6b0d7af044fd180ad1de1714
Noah Crowley <noahwillcrow@gmail.com>
2020-04-06 14:45:42 -0400

    delete unnecessary functions

--- tests/src/functions/createFakeCamera.lua
+++
@@ -1,5 +0,0 @@
-return function (props)
-	return {
-		ViewportSize = props and props.ViewportSize or Vector2.new(1, 1)
-	}
-end
--- tests/src/functions/createFakeGuiService.lua
+++
@@ -1,7 +0,0 @@
-return function (props, guiInset)
-	return {
-		GetGuiInset = function(self)
-			return guiInset or { Vector2.new(0, 0), Vector2.new(0, 0) }
-		end
-	}
-end
