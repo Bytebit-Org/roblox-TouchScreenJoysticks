c5f7ae2a7656a9f3370c6eff8c92d18b082d6c11
Noah Crowley <noahwillcrow@gmail.com>
2020-04-06 19:43:50 -0400

    add two new tests

--- tests/src/JoysticksManager.spec.lua
+++ tests/src/JoysticksManager.spec.lua
@@ -140,5 +140,76 @@ return function()
 				destroyerJoysticksManager:destroyJoystick(joystick)
 			end).to.throw()
 		end)
+
+		it("should throw when destroying the same joystick twice", function()
+			local currentCameraGetter = function ()
+				return a.fake()
+			end
+
+			local guiService = a.fake()
+			local screenGuisParent = Instance.new("Folder")
+			local userInputService = a.fake()
+
+			local joysticksManager = JoysticksManager.new(
+				currentCameraGetter,
+				guiService,
+				screenGuisParent,
+				userInputService
+			)
+
+			local joystick = joysticksManager:createJoystick({
+				activationRegion = a.fake(),
+				gutterRadiusInPixels = 10,
+				inactiveCenterPoint = Vector2.new(0, 0),
+				renderer = a.fake(),
+				relativeThumbRadius = 0.5
+			})
+
+			joysticksManager:destroyJoystick(joystick)
+
+			expect(function()
+				joysticksManager:destroyJoystick(joystick)
+			end).to.throw()
+		end)
+
+		it("should activate the appropriate joystick when there is only one", function()
+			local currentCameraGetter = function ()
+				local camera = a.fake()
+				camera.ViewportSize = Vector2.new(100, 100)
+				return camera
+			end
+
+			local guiService = a.fake()
+			local screenGuisParent = Instance.new("Folder")
+			local userInputService = a.fake()
+
+			a.callTo(guiService, "GetGuiInset", guiService):returns(Vector2.new(), Vector2.new())
+
+			userInputService.TouchStarted = createFakeSignal()
+
+			local joysticksManager = JoysticksManager.new(
+				currentCameraGetter,
+				guiService,
+				screenGuisParent,
+				userInputService
+			)
+
+			local activationRegion = a.fake()
+			a.callTo(activationRegion, "isPointInRegion", activationRegion, fitumi.wildcard):returns(true)
+
+			local joystick = joysticksManager:createJoystick({
+				activationRegion = activationRegion,
+				gutterRadiusInPixels = 10,
+				inactiveCenterPoint = Vector2.new(0, 0),
+				initializedEnabled = true,
+				initializedVisible = true,
+				renderer = a.fake(),
+				relativeThumbRadius = 0.5
+			})
+
+			userInputService.TouchStarted:Fire(createFakeInputObject(), false)
+
+			expect(joystick.isActive).to.equal(true)
+		end)
     end)
 end
