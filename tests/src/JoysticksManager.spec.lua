e628f77f28e270985ee2c8007cb77ecbf8f043dc
Noah Crowley <noahwillcrow@gmail.com>
2020-04-06 14:18:38 -0400

    update test

--- tests/src/JoysticksManager.spec.lua
+++ tests/src/JoysticksManager.spec.lua
@@ -1,22 +1,24 @@
 return function()
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+	local fitumi = require(ReplicatedStorage:WaitForChild("fitumi"))
     local TS = require(ReplicatedStorage:WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
 	local JoysticksManager = TS.import(script, ReplicatedStorage, "TS", "Implementation", "JoysticksManager").JoysticksManager;
 
-	local createFakeCamera = require(ReplicatedStorage.Tests.functions.createFakeCamera)
-	local createFakeGuiService = require(ReplicatedStorage.Tests.functions.createFakeGuiService)
+	local a = fitumi.a
+
 	local createFakeInputObject = require(ReplicatedStorage.Tests.functions.createFakeInputObject)
-	local createFakeUserInputService = require(ReplicatedStorage.Tests.functions.createFakeUserInputService)
+	local createFakeSignal = require(ReplicatedStorage.Tests.functions.createFakeSignal)
 
     describe("JoysticksManager", function()
-		it("should be safely instantiable and fulfill the interface", function()
+		it("should be constructable and fulfill its interface", function()
 			local currentCameraGetter = function ()
-				return createFakeCamera()
+				return a.fake()
 			end
-			local guiService = createFakeGuiService()
+
+			local guiService = a.fake()
 			local screenGuisParent = Instance.new("Folder")
-			local userInputService = createFakeUserInputService()
+			local userInputService = a.fake()
 
 			local joysticksManager = nil
 			expect(function ()
@@ -32,13 +34,14 @@ return function()
             expect(joysticksManager.destroyJoystick).to.be.a("function")
 		end)
 
-		it("register joysticks correctly", function()
+		it("should add and destroy joysticks", function()
 			local currentCameraGetter = function ()
-				return createFakeCamera()
+				return a.fake()
 			end
-			local guiService = createFakeGuiService()
+
+			local guiService = a.fake()
 			local screenGuisParent = Instance.new("Folder")
-			local userInputService = createFakeUserInputService()
+			local userInputService = a.fake()
 
 			local joysticksManager = JoysticksManager.new(
 				currentCameraGetter,
@@ -47,10 +50,55 @@ return function()
 				userInputService
 			)
 
-            userInputService.TouchStarted:Fire(
-                createFakeInputObject(),
-                false
-            )
-        end)
+			local joystick = nil
+			expect(function()
+				joystick = joysticksManager:createJoystick({
+					activationRegion = a.fake(),
+					gutterRadiusInPixels = 10,
+					inactiveCenterPoint = Vector2.new(0, 0),
+					renderer = a.fake(),
+					relativeThumbRadius = 0.5
+				})
+			end).never.to.throw()
+
+			expect(function()
+				joysticksManager:destroyJoystick(joystick)
+			end).never.to.throw()
+		end)
+
+		it("should throw when attempting to destroy a joystick from another manager", function()
+			local currentCameraGetter = function ()
+				return a.fake()
+			end
+
+			local guiService = a.fake()
+			local screenGuisParent = Instance.new("Folder")
+			local userInputService = a.fake()
+
+			local creatorJoysticksManager = JoysticksManager.new(
+				currentCameraGetter,
+				guiService,
+				screenGuisParent,
+				userInputService
+			)
+			local destroyerJoysticksManager = JoysticksManager.new(
+				currentCameraGetter,
+				guiService,
+				screenGuisParent,
+				userInputService
+			)
+
+			local joystick = creatorJoysticksManager:createJoystick({
+				activationRegion = a.fake(),
+				gutterRadiusInPixels = 10,
+				inactiveCenterPoint = Vector2.new(0, 0),
+				renderer = a.fake(),
+				relativeThumbRadius = 0.5
+			})
+
+			expect(function()
+				destroyerJoysticksManager:destroyJoystick(joystick)
+			end).to.throw()
+		end)
     end)
 end
