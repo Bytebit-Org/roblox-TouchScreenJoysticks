e3eb0e8708389b2370c1f42e36d5aca5ba9137b0
Noah Crowley <noahwillcrow@gmail.com>
2020-04-07 01:27:41 -0400

    clean up

--- src/Implementation/GuiWindowRegions/CircleGuiWindowRegion.ts
+++ src/Implementation/GuiWindowRegions/CircleGuiWindowRegion.ts
@@ -2,23 +2,23 @@ import { IGuiWindowRegion } from "../../Interfaces/IGuiWindowRegion";
 
 /** An implementation of `IGuiWindowRegion` that describes a circular region of the viewport */
 export class CircleGuiWindowRegion implements IGuiWindowRegion {
-    private readonly centerPoint: Vector2;
-    private readonly radiusInPixels: number;
+	private readonly centerPoint: Vector2;
+	private readonly radiusInPixels: number;
 
-    /**
-     * Creates a new instance
-     * @param centerPoint A `Vector2` describing the center point in the viewport of the region
-     * @param radiusInPixels A `number` describing the radius in pixels of the region
-     */
-    public constructor(centerPoint: Vector2, radiusInPixels: number) {
-        this.centerPoint = centerPoint;
-        this.radiusInPixels = radiusInPixels;
-    }
+	/**
+	 * Creates a new instance
+	 * @param centerPoint A `Vector2` describing the center point in the viewport of the region
+	 * @param radiusInPixels A `number` describing the radius in pixels of the region
+	 */
+	public constructor(centerPoint: Vector2, radiusInPixels: number) {
+		this.centerPoint = centerPoint;
+		this.radiusInPixels = radiusInPixels;
+	}
 
-    public isPointInRegion(point: Vector2) {
-        const displacement = this.centerPoint.sub(point);
-        const isWithinRadius = displacement.Magnitude <= this.radiusInPixels;
+	public isPointInRegion(point: Vector2) {
+		const displacement = this.centerPoint.sub(point);
+		const isWithinRadius = displacement.Magnitude <= this.radiusInPixels;
 
-        return isWithinRadius;
-    }
+		return isWithinRadius;
+	}
 }
