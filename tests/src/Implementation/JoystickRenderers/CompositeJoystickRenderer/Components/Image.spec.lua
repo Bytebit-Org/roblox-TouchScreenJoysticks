2eb578d409e51a52149dd4fb7f85cb1d54a4b3bb
Noah Crowley <noahwillcrow@gmail.com>
2020-04-07 21:32:53 -0400

    spec the two rendeer component

--- tests/src/Implementation/JoystickRenderers/CompositeJoystickRenderer/Components/Image.spec.lua
+++ tests/src/Implementation/JoystickRenderers/CompositeJoystickRenderer/Components/Image.spec.lua
@@ -1,14 +1,112 @@
 return function()
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-	local fitumi = require(ReplicatedStorage:WaitForChild("fitumi"))
-    local TS = require(ReplicatedStorage:WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
-	local Image = TS.import(script, ReplicatedStorage, "TS", "Implementation", "JoystickRenderers", "CompositeJoystickRenderer", "Components", "Image").Image;
+    local TS = require(ReplicatedStorage:WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
+	local Image = TS.import(script, ReplicatedStorage, "TS", "Implementation", "JoystickRenderers", "CompositeJoystickRenderer", "Components", "Image").Image
 
-	local a = fitumi.a
+	local testAssetPath = "rbxassetid://4798571099"
 
-	local createFakeSignal = require(ReplicatedStorage.Tests.functions.createFakeSignal)
-	local createNoopSignal = require(ReplicatedStorage.Tests.functions.createNoopSignal)
+	describe("Image CompositeJoystickRenderer Component", function()
+		it("should be constructable and fulfill its interface", function()
+			local image;
 
-	-- TODO
+			expect(function()
+				image = Image.new(testAssetPath, Color3.new(0, 0, 0), 0)
+			end).never.to.throw()
+
+			expect(image.destroy).to.be.a("function")
+			expect(image.getParentableInstance).to.be.a("function")
+			expect(image.hide).to.be.a("function")
+			expect(image.render).to.be.a("function")
+		end)
+
+		it("should create a matching image label to its inputs", function()
+			for _ = 1, 30 do
+				local color = Color3.new(math.random(), math.random(), math.random())
+				local transparency = math.random()
+				local image = Image.new(testAssetPath, color, transparency)
+
+				expect(image.imageLabel).to.be.ok()
+				expect(image.imageLabel.Image).to.equal(testAssetPath)
+				expect(image.imageLabel.ImageColor3).to.equal(color)
+				expect(image.imageLabel.ImageTransparency).to.be.near(transparency, 1/128)
+			end
+		end)
+
+		it("should be destroyable and all public methods should throw", function()
+			local image = Image.new(testAssetPath, Color3.new(0, 0, 0), 0)
+
+			image:destroy()
+
+			expect(function()
+				image:getParentableInstance()
+			end).to.throw()
+
+			expect(function()
+				image:hide()
+			end).to.throw()
+
+			expect(function()
+				image:render(
+					--[[ position ]] UDim2.new(),
+					--[[ size ]] UDim2.new(),
+					--[[ zIndex ]] 1,
+					--[[ parent ]] Instance.new("Folder")
+				)
+			end).to.throw()
+		end)
+
+		it("should return its own image label as the parentable instance", function()
+			local image = Image.new(testAssetPath, Color3.new(0, 0, 0), 0)
+
+			image:render(
+				--[[ position ]] UDim2.new(),
+				--[[ size ]] UDim2.new(),
+				--[[ zIndex ]] 1,
+				--[[ parent ]] Instance.new("Folder")
+			)
+
+			local parentableInstance = image:getParentableInstance()
+
+			expect(parentableInstance).to.equal(image.imageLabel)
+		end)
+
+		it("should hide its image label by changing the parent to nil", function()
+			local image = Image.new(testAssetPath, Color3.new(0, 0, 0), 0)
+
+			image:render(
+				--[[ position ]] UDim2.new(),
+				--[[ size ]] UDim2.new(),
+				--[[ zIndex ]] 1,
+				--[[ parent ]] Instance.new("Folder")
+			)
+
+			image:hide()
+
+			expect(image.imageLabel.Parent).never.to.be.ok()
+		end)
+
+		it("should render its image label by changing the properties as dictated", function()
+			local image = Image.new(testAssetPath, Color3.new(0, 0, 0), 0)
+
+			for _ = 1, 30 do
+				local position = UDim2.new(math.random(), 0, math.random(), 0)
+				local size = UDim2.new(math.random(), 0, math.random(), 0)
+				local zIndex = math.floor(math.random() * 10) + 1
+				local parent = Instance.new("Folder")
+
+				image:render(
+					--[[ position ]] position,
+					--[[ size ]] size,
+					--[[ zIndex ]] zIndex,
+					--[[ parent ]] parent
+				)
+
+				expect(image.imageLabel.Position).to.equal(position)
+				expect(image.imageLabel.Size).to.equal(size)
+				expect(image.imageLabel.ZIndex).to.equal(zIndex)
+				expect(image.imageLabel.Parent).to.equal(parent)
+			end
+		end)
+	end)
 end
--- tests/src/Implementation/JoystickRenderers/CompositeJoystickRenderer/Components/SolidFilledCircle.spec.lua
+++ tests/src/Implementation/JoystickRenderers/CompositeJoystickRenderer/Components/SolidFilledCircle.spec.lua
@@ -1,14 +1,109 @@
 return function()
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-	local fitumi = require(ReplicatedStorage:WaitForChild("fitumi"))
-    local TS = require(ReplicatedStorage:WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"));
-	local SolidFilledCircle = TS.import(script, ReplicatedStorage, "TS", "Implementation", "JoystickRenderers", "CompositeJoystickRenderer", "Components", "SolidFilledCircle").SolidFilledCircle;
+    local TS = require(ReplicatedStorage:WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
+	local SolidFilledCircle = TS.import(script, ReplicatedStorage, "TS", "Implementation", "JoystickRenderers", "CompositeJoystickRenderer", "Components", "SolidFilledCircle").SolidFilledCircle
 
-	local a = fitumi.a
+	describe("SolidFilledCicle CompositeJoystickRenderer Component", function()
+		it("should be constructable and fulfill its interface", function()
+			local solidFilledCircle;
 
-	local createFakeSignal = require(ReplicatedStorage.Tests.functions.createFakeSignal)
-	local createNoopSignal = require(ReplicatedStorage.Tests.functions.createNoopSignal)
+			expect(function()
+				solidFilledCircle = SolidFilledCircle.new(Color3.new(0, 0, 0), 0)
+			end).never.to.throw()
 
-	-- TODO
+			expect(solidFilledCircle.destroy).to.be.a("function")
+			expect(solidFilledCircle.getParentableInstance).to.be.a("function")
+			expect(solidFilledCircle.hide).to.be.a("function")
+			expect(solidFilledCircle.render).to.be.a("function")
+		end)
+
+		it("should create a matching image label to its inputs", function()
+			for _ = 1, 30 do
+				local color = Color3.new(math.random(), math.random(), math.random())
+				local transparency = math.random()
+				local solidFilledCircle = SolidFilledCircle.new(color, transparency)
+
+				expect(solidFilledCircle.imageLabel).to.be.ok()
+				expect(solidFilledCircle.imageLabel.ImageColor3).to.equal(color)
+				expect(solidFilledCircle.imageLabel.ImageTransparency).to.be.near(transparency, 1/128)
+			end
+		end)
+
+		it("should be destroyable and all public methods should throw", function()
+			local solidFilledCircle = SolidFilledCircle.new(Color3.new(0, 0, 0), 0)
+
+			solidFilledCircle:destroy()
+
+			expect(function()
+				solidFilledCircle:getParentableInstance()
+			end).to.throw()
+
+			expect(function()
+				solidFilledCircle:hide()
+			end).to.throw()
+
+			expect(function()
+				solidFilledCircle:render(
+					--[[ position ]] UDim2.new(),
+					--[[ size ]] UDim2.new(),
+					--[[ zIndex ]] 1,
+					--[[ parent ]] Instance.new("Folder")
+				)
+			end).to.throw()
+		end)
+
+		it("should return its own image label as the parentable instance", function()
+			local solidFilledCircle = SolidFilledCircle.new(Color3.new(0, 0, 0), 0)
+
+			solidFilledCircle:render(
+				--[[ position ]] UDim2.new(),
+				--[[ size ]] UDim2.new(),
+				--[[ zIndex ]] 1,
+				--[[ parent ]] Instance.new("Folder")
+			)
+
+			local parentableInstance = solidFilledCircle:getParentableInstance()
+
+			expect(parentableInstance).to.equal(solidFilledCircle.imageLabel)
+		end)
+
+		it("should hide its image label by changing the parent to nil", function()
+			local solidFilledCircle = SolidFilledCircle.new(Color3.new(0, 0, 0), 0)
+
+			solidFilledCircle:render(
+				--[[ position ]] UDim2.new(),
+				--[[ size ]] UDim2.new(),
+				--[[ zIndex ]] 1,
+				--[[ parent ]] Instance.new("Folder")
+			)
+
+			solidFilledCircle:hide()
+
+			expect(solidFilledCircle.imageLabel.Parent).never.to.be.ok()
+		end)
+
+		it("should render its image label by changing the properties as dictated", function()
+			local solidFilledCircle = SolidFilledCircle.new(Color3.new(0, 0, 0), 0)
+
+			for _ = 1, 30 do
+				local position = UDim2.new(math.random(), 0, math.random(), 0)
+				local size = UDim2.new(math.random(), 0, math.random(), 0)
+				local zIndex = math.floor(math.random() * 10) + 1
+				local parent = Instance.new("Folder")
+
+				solidFilledCircle:render(
+					--[[ position ]] position,
+					--[[ size ]] size,
+					--[[ zIndex ]] zIndex,
+					--[[ parent ]] parent
+				)
+
+				expect(solidFilledCircle.imageLabel.Position).to.equal(position)
+				expect(solidFilledCircle.imageLabel.Size).to.equal(size)
+				expect(solidFilledCircle.imageLabel.ZIndex).to.equal(zIndex)
+				expect(solidFilledCircle.imageLabel.Parent).to.equal(parent)
+			end
+		end)
+	end)
 end
