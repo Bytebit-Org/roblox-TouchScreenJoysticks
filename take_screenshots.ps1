048d8f5ae0b4bbd61c14d16c4af96f072cec9422
Noah Crowley <ncrowley@roblox.com>
2020-04-11 00:26:29 -0400

    custom screenshot task

--- take_screenshots.ps1
+++ take_screenshots.ps1
@@ -1,6 +1,23 @@
 while($true)
 {
     $i++
-    .\\boxcutter.exe -f ".\\screenshots\$i.png"
-    Start-Sleep 10
+    $outputFile = ".\\screenshots\$i.png"
+
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+
+    $screenshotImage = New-Object System.Drawing.Bitmap $Width, $Height
+
+    $graphicObject = [System.Drawing.Graphics]::FromImage($screenshotImage)
+    $graphicObject.CopyFromScreen($Left, $Top, 0, 0, $screenshotImage.Size)
+
+    $screenshotImage.Save($outputFile)
+
+    Start-Sleep -s 5
 }
